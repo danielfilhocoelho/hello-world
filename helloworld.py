ba9e71f03c69100d4af2ef2eb6f9e3bfe7b76533
daniel ramos <danielfilhocoelho@hotmail.com>
2021-01-30 18:09:35 -0300

    ososos

--- helloworld.py
+++ helloworld.py
@@ -3,11 +3,8 @@ b = 0
 print('hello world')
 c = 0
 print('novo commit')
-<<<<<<< HEAD
 if(a + b > 5):
     print('ok')
 print('some bullshit')
-=======
 if(a > 2):
   print('sim')
->>>>>>> acb7b9565289685c3f89b35e77faf3e6cbd21602
